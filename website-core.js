fd8568e1d8252c520ca3e1cca1498fa93991773d
nathaliejeans <70771094+nathaliejeans@users.noreply.github.com>
2021-01-09 21:23:18 -0500

    Add files via upload

---
+++ website-core.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#plant").click(() => window.open("https://github.com/nathaliejeans/verticalfarming", "_blank"));
+    $("#stationary").click(() => window.open("https://github.com/digit-recognition", "_blank"));
+    $("#money").click(() => window.open("projects/educents.pdf", "_blank"));
+    $("#table").click(() => window.open("https://github.com/nathaliejeans/employee-analyst", "_blank"));
+    $("#cup").click(() => window.open("https://github.com/nathaliejeans/yolov3", "_blank"));
+
+    $("#linkedin").click(() => window.open("https://www.linkedin.com/in/nathaliejeans/", "_blank"));
+    $("#medium").click(() => window.open("https://medium.com/@nathaliejeans", "_blank"));
+    $("#github").click(() => window.open("https://github.com/nathaliejeans", "_blank"));
+    $("#mail").click(() => window.open("mailto:nathaliejeans7@gmail.com", "_blank"));
+
+    $("#wealthsimple").click(() => window.open("projects/wealthsimple.pdf", "_blank"));
+    $("#educents").click(() => window.open("projects/educents.pdf", "_blank"));
+    $("#walmart").click(() => window.open("projects/walmart.pdf", "_blank"));
+    $("#uniform").click(() => window.open("projects/uniform.pdf", "_blank"));
+    $("#gans").click(() => window.open("projects/gans.pdf", "_blank"));
+    $("#exobi").click(() => window.open("projects/exobi.pdf", "_blank"));
+});
